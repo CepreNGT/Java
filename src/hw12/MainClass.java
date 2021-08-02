a7ac80da424a82ab1ec79a88045e1de1a00dc5ef
CepreN_GT <malitevs@gmail.com>
2021-08-02 15:15:20 +0500

    hw12 redo

--- src/hw12/MainClass.java
+++ src/hw12/MainClass.java
@@ -1,5 +1,7 @@
 package hw12;
 
+import sun.java2d.loops.GraphicsPrimitive;
+
 import java.util.Arrays;
 
 public class MainClass {
@@ -25,7 +27,7 @@ public class MainClass {
         System.out.println(endTime - startTime);
     }
 
-    public static void twoThreads(){
+    public static void twoThreads() {
         float[] arr = new float[SIZE];
         Arrays.fill(arr, 1);
         long startTime = System.currentTimeMillis();
@@ -33,11 +35,25 @@ public class MainClass {
         float[] arr2 = new float[HALF];
         System.arraycopy(arr, 0, arr1, 0, HALF);
         System.arraycopy(arr, HALF, arr2, 0, HALF);
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = (float) (arr[i] * Math.sin(0.2f + (float) i / 5) *
-                    Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
-            arr2[i] = (float) (arr[i] * Math.sin(0.2f + (float) i / 5) *
-                    Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + (float) i / 5) *
+                        Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < arr2.length; i++) {
+                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + (float) i / 5) *
+                        Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.arraycopy(arr1, 0, arr, 0, HALF);
         System.arraycopy(arr2, 0, arr, HALF, HALF);
